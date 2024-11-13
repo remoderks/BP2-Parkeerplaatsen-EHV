84b5017da473b0ba235ae23dca676c685d092cc7
Remo Derks <remo_derks@hotmail.com>
2024-11-13 18:06:20 +0100

    no message

--- BP2 Parkeerplaatsen EHV/src/main/java/com/bp2parkeerplaatsenehv/Menubar/MenuBar.java
+++ BP2 Parkeerplaatsen EHV/src/main/java/com/bp2parkeerplaatsenehv/Menubar/MenuBar.java
@@ -1,4 +1,19 @@
 package com.bp2parkeerplaatsenehv.Menubar;
 
 public class MenuBar {
+    private String menuName;
+    private String menuIcon;
+
+    public MenuBar(String menuName, String menuIcon) {
+        this.menuName = menuName;
+        this.menuIcon = menuIcon;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public String getMenuIcon() {
+        return menuIcon;
+    }
 }
